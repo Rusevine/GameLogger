811fbd295bec22051890db3353a65a99d9b97001
Wiljay <wiljay.flores@gmail.com>
2018-08-22 13:10:32 -0400

    Created Objc File DetailView Controller

---
+++ GameLogger/Controllers/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  GameLogger
+//
+//  Created by Wiljay Flores on 2018-08-22.
+//  Copyright © 2018 Henry Cooper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
